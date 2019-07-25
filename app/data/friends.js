784139694c25bcfbff9e041461fd28db5ee55581
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-07-24 21:05:36 -0700

    updated with working files, still working on on submit on click button

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+
+    {
+        name: "Chris",
+        photo: "https://loremflickr.com/320/240/cat",
+        scores: [
+            "5",
+            "1",
+            "3",
+            "2",
+            "1",
+            "4",
+            "3",
+            "5",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Tai",
+        photo: "https://loremflickr.com/320/240/dog",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "3",
+            "2",
+            "1",
+            "5",
+            "4",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Calvin",
+        photo: "https://loremflickr.com/320/240/monkey",
+        scores: [
+            "1",
+            "5",
+            "1",
+            "4",
+            "5",
+            "2",
+            "1",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var app = express();
 
+
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/apiRoutes.js")(app);
-require("app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: http://localhost:" + PORT);
