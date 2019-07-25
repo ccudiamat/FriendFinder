2927daa4272aae8a183ebe07da07ce456bff012e
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-07-24 21:53:21 -0700

    updated files with completed working code

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var app = express();
-
+var path = require("path");
+var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+app.use(bodyParser.json({ type: "application/*+json" }))
+
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }))
+
+app.use(bodyParser.text({ type: "text/html" }))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
